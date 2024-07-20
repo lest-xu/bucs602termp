3770d6189d45f7b69705b89e6e7e89b6e3bdfb3e
“lest”-xu <autodataxx@gmail.com>
2024-07-20 10:37:07 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-// other modules
-var displayproducts = require("./displayProducts");
-var addProduct = require("./addProduct");
-var saveProduct = require("./saveProduct");
-var editProduct = require("./editProduct");
-var saveAfterEdit = require("./saveAfterEdit");
-var deleteProduct = require("./deleteProduct");
-var deleteProductAfterConfirm = require("./deleteProductAfterConfirm");
+// order modules
+var displayOrders = require("./order/displayOrders");
+var addOrder = require("./order/addOrder");
+var saveOrder = require("./order/saveOrder");
+var editOrder = require("./order/editOrder");
+var saveOrderAfterEdit = require("./order/saveOrderAfterEdit");
+var deleteOrder = require("./order/deleteOrder");
+var deleteOrderAfterConfirm = require("./order/deleteOrderAfterConfirm");
 
-/// order routes
+// customer modules
+var displayCustomers = require("./customer/displayCustomers");
+var addCustomer = require("./customer/addCustomer");
+var saveCustomer = require("./customer/saveCustomer");
+var editCustomer = require("./customer/editCustomer");
+var saveCustomerAfterEdit = require("./customer/saveCustomerAfterEdit");
+var deleteCustomer = require("./customer/deleteCustomer");
+var deleteCustomerAfterConfirm = require("./customer/deleteCustomerAfterConfirm");
 
-/// customer routes
+// product modules
+var displayProducts = require("./product/displayProducts");
+var addProduct = require("./product/addProduct");
+var saveProduct = require("./product/saveProduct");
+var editProduct = require("./product/editProduct");
+var saveAfterEdit = require("./product/saveAfterEdit");
+var deleteProduct = require("./product/deleteProduct");
+var deleteProductAfterConfirm = require("./product/deleteProductAfterConfirm");
 
-
-/// product routes
-// router specs
+// shopping cart home page
 router.get('/', function (req, res, next) {
-  res.redirect('/products');
+  res.redirect('/');
 });
 
-router.get('/products', displayproducts);
+// order routes
 
+// customer routes
+
+
+// product routes
+router.get('/products', displayProducts);
 router.get('/products/add', addProduct);
 router.post('/products/add', saveProduct);
-
 router.get('/products/edit/:id', editProduct);
 router.post('/products/edit/', saveAfterEdit);
-
 router.get('/products/delete/:id', deleteProduct);
 router.post('/products/delete', deleteProductAfterConfirm);
 
